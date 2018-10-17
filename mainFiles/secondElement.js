99432e42042d4e99d46ef4cf722c6d58bff8a89a
bsrushti <bsrushti@in-bsrushti.local>
2018-10-17 10:59:44 +0530

    modified test and source of select second element
    added test for two and four elements

--- mainFiles/secondElement.js
+++ mainFiles/secondElement.js
@@ -1,11 +1,15 @@
+let numberList = [1,5,2,7,1];
 const selectSecondElement = function(numberList) {
   let secondElementList = [];
-  for(let index = 2; index < numberList.length; index++) {
-    secondElementList = secondElementList + numberList[index];
+  let arrIndex = 0;
+  for(let index = 2; index < numberList.length; index+=2) {
+    secondElementList[arrIndex] = numberList[index];
+    arrIndex ++;
   }
+  console.log(secondElementList);
   return secondElementList;
 }
-
+selectSecondElement(numberList);
 exports.selectSecondElement = selectSecondElement;
 
 
